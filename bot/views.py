ecdfeca14c05f3187a8b8b8571d13cf159c007bf
noahwekesa <noahwekesa0@gmail.com>
2024-06-02 18:26:27 +0300

    csrf excempy

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def slack_events_endpoint(request):
-    print(request.body)
+    print(request.body, request.method)
     return HttpResponse("<h1>Hello world</h1>", status=200)
